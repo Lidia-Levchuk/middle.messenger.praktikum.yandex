e3ba643bb2079d353cba13a8cfbb66c993275b9d
Lidiya_Levchuk <lidiya.levchuk1998@gmail.com>
2024-11-20 00:59:27 +0300

    Добавлены файлы выполнения задания. Часть 3

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export {LoginPage} from "./login"
+export {RegistrationPage} from "./registration"
+export {ErrorPage} from "./error"
+export {ProfilePage} from "./profile"
+export {ListPage} from "./list"
+export {NavigatePage} from "./navigate"
