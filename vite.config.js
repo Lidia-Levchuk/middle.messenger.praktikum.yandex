732c0affd029b0098633a4ec8563a5de57703ba3
Lidiya_Levchuk <lidiya.levchuk1998@gmail.com>
2024-11-21 22:31:59 +0300

    Внесены правки после неудачного деплоя для возвращения к исходному, принятому на ревью состоянию

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import handlebars from 'vite-plugin-handlebars';
-
-export default defineConfig({
-  plugins: [handlebars()],
-  server: {
-    port: 4000,
-  },
-});
